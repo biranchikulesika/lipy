49d0cbc19320a949164a36b7436cc66d9da067d8
Birannchi Kulesika <biranchi.dev@proton.me>
2026-07-09 13:00:12 +0530

    Update sitemap.ts

--- frontend/app/sitemap.ts
+++ frontend/app/sitemap.ts
@@ -1,8 +1,9 @@
 import { MetadataRoute } from 'next';
- 
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://lipy.vercel.app";
-  
+  const baseUrl =
+    process.env.NEXT_PUBLIC_SITE_URL ?? 'http://localhost:3000';
+
   return [
     {
       url: `${baseUrl}/`,
